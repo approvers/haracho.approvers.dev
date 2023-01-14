33c0d21f02a0fdb770833a81053b6e463c83509e
m2en <me@m2en.dev>
2023-01-15 05:07:50 +0900

    docs: add execution environment

--- sidebars.js
+++ sidebars.js
@@ -2,5 +2,18 @@
 
 module.exports = {
   docs: ['intro'],
-  dev_docs: ['dev_docs/index'],
+  dev_docs: [
+    'dev_docs/index',
+    {
+      type: 'category',
+      label: '実行',
+      collapsed: false,
+      items: [
+        'dev_docs/running/index',
+        'dev_docs/running/local',
+        'dev_docs/running/docker-useImage',
+        'dev_docs/running/docker-useCompose',
+      ],
+    },
+  ],
 };
