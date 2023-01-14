2b15b4d4082a54b1428313d9f7881093d47b30e7
m2en <me@m2en.dev>
2023-01-15 02:31:10 +0900

    feat: top-page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,22 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components';
 
-import styles from './index.module.css';
+import styles from '../css/index.module.css';
+
+const buttonInput = "button button--outline button--primary"
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">OreOreBot2</h1>
-        <p className="hero__subtitle">限界開発鯖民が愛するDiscord Bot</p>
+        <p className="hero__subtitle">{useDocusaurusContext().siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className={clsx("button button--secondary button--lg", styles.goToPage)} to="/docs/intro">
+          <Link className={clsx(buttonInput, styles.buttonsItem)} to="/docs/intro">
             ドキュメント
           </Link>
-          <Link className={clsx("button button--secondary button--lg", styles.goToPage)} to="/docs/intro">
+          <Link className={clsx(buttonInput, styles.buttonsItem)} to="/docs/intro">
             開発ガイド
           </Link>
         </div>
@@ -34,9 +34,6 @@ export default function Home(): JSX.Element {
       title={siteConfig.title}
       description={siteConfig.tagline}>
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
     </Layout>
   );
 }
