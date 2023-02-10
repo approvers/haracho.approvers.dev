ac5104df87c129da528c4db7879432447d317023
MikuroXina <ryosukadnak@gmail.com>
2023-02-10 23:25:23 +0900

    chore: Setup eslint

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,8 @@
-import pkg from './package.json' assert { type: 'json' }
+import pkg from './package.json' assert { type: 'json' };
 
 export default {
   logo: <span>OreOreBot2 Documents</span>,
   project: {
-    link: pkg.repository,
-  },
-}
+    link: pkg.repository
+  }
+};
