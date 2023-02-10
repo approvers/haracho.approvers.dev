0d3cbd4647e092f9189c6dad340567074e4d502b
Mikuroさいな <ryosukadnak@gmail.com>
2023-02-10 23:42:34 +0900

    chore: lint のセットアップ (#38)

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+  themeConfig: './theme.config.tsx'
+});
 
-module.exports = withNextra()
+module.exports = withNextra();
