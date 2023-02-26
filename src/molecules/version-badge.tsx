25753bb1a6178bcb14e4959a05777ebf10673460
MikuroXina <ryosukadnak@gmail.com>
2023-02-26 22:47:33 +0900

    Fix style with module css

--- src/molecules/version-badge.tsx
+++ src/molecules/version-badge.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import { Badge } from '../atoms/badge';
+import style from './version-badge.module.css';
+
 export interface VersionProps {
   children: React.ReactNode;
 }
 
 export const VersionBadge = ({ children }: VersionProps): JSX.Element => {
-  return <span className="badge badge--success">{children}</span>;
+  return (
+    <span className={style.version}>
+      <Badge>{children}</Badge>
+    </span>
+  );
 };
